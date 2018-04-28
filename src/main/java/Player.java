3613ce43f92d4a5e96c71405e27dd80e49ca3096
max31051980 <36488974+max31051980@users.noreply.github.com>
2018-04-29 02:04:51 +0300

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,1 +1,30 @@
+public class Player {
+    private int valueCount;
+    private int cardCount;
+    Card[] deckArray = new Card[52];
+    private int sum;
 
+    public void addCard(Card a) {
+        deckArray[cardCount] = a;
+        cardCount++;
+    }
+
+    public int getCardCount() {
+        return cardCount;
+    }
+
+    public Card getCard(int a) {
+        return deckArray[a];
+    }
+
+    public int getCardValue(int a) {
+        valueCount = deckArray[a].valueOf();
+        return valueCount;
+    }
+
+    public void getValue(int a) {
+        for(int i = 0; i < deckArray.length; i++) {
+            sum += valueCount;
+        }
+    }
+}
